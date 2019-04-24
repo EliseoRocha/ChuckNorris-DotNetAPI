e49706a7d1d50d291129d37f0d2c59450d6d86e0
Eliseo Rocha <rochaeliseo91@gmail.com>
2019-04-24 12:47:29 -0700

    Added NuGet package and rearranged code for combobox.

---
+++ ChuckNorris/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ChuckNorris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GetJokeButton = new System.Windows.Forms.Button();
+            this.CategoriesComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // GetJokeButton
+            // 
+            this.GetJokeButton.Location = new System.Drawing.Point(111, 97);
+            this.GetJokeButton.Name = "GetJokeButton";
+            this.GetJokeButton.Size = new System.Drawing.Size(172, 70);
+            this.GetJokeButton.TabIndex = 0;
+            this.GetJokeButton.Text = "Get Joke";
+            this.GetJokeButton.UseVisualStyleBackColor = true;
+            this.GetJokeButton.Click += new System.EventHandler(this.GetJokeButton_ClickAsync);
+            // 
+            // CategoriesComboBox
+            // 
+            this.CategoriesComboBox.FormattingEnabled = true;
+            this.CategoriesComboBox.Location = new System.Drawing.Point(111, 31);
+            this.CategoriesComboBox.Name = "CategoriesComboBox";
+            this.CategoriesComboBox.Size = new System.Drawing.Size(172, 21);
+            this.CategoriesComboBox.TabIndex = 1;
+            this.CategoriesComboBox.SelectedIndexChanged += new System.EventHandler(this.CategoriesComboBox_SelectedIndexChangedAsync);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 232);
+            this.Controls.Add(this.CategoriesComboBox);
+            this.Controls.Add(this.GetJokeButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_LoadAsync);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GetJokeButton;
+        private System.Windows.Forms.ComboBox CategoriesComboBox;
+    }
+}
+
---
+++ ChuckNorris/Form1.cs
@@ -0,0 +1,40 @@
+﻿using ChuckNorrisAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChuckNorris
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void GetJokeButton_ClickAsync(object sender, EventArgs e)
+        {
+            Joke joke = await ChuckNorrisClient.GetRandomJoke();
+            MessageBox.Show(joke.JokeText);
+        }
+
+        private async void CategoriesComboBox_SelectedIndexChangedAsync(object sender, EventArgs e)
+        {
+            //IEnumerable<string> categories = await ChuckNorrisClient.GetCategories();
+            //CategoriesComboBox.DataSource = categories;
+            //CategoriesComboBox.DisplayMember = "Categories";
+        }
+
+        private async void Form1_LoadAsync(object sender, EventArgs e)
+        {
+            IEnumerable<string> categories = await ChuckNorrisClient.GetCategories();
+            CategoriesComboBox.DataSource = categories;
+        }
+    }
+}
